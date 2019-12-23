e275079b78cbe021a37f8be3a019e9120bf9e7d1
alexeyhorkin <alexeykhorkin12@gmail.com>
2019-12-23 19:23:41 +0300

    was modify for conference

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ import numpy as np
 import json
 import random as rd
 import tensorflow as tf
+import tensorflow.contrib.slim as slim
+from tensorflow.python.ops.parallel_for.gradients import jacobian
+import matplotlib.gridspec as gridspec
 raw_dataset_path="ecg_data_200\\ecg_data_200.json" #файл с датасетом
-
 def Openf(link):
     # function for opening some json files
     f = open(link,'r')
@@ -38,12 +40,13 @@ def Create_mask_static(start,size, batch_size):
 
 
 data = Openf(raw_dataset_path)
-Leads1= data["50519553"]["Leads"]
 
-def MyGeteratorData(batch_size):
+Leads1= data["50519553"]["Leads"]
+# Leads1= data["50488354"]["Leads"]
+
+def MyGeteratorData(batch_size, patient):
 	f = True
-	# Leads1 = data[str(rd.sample(data.keys(), 1)[0])]["Leads"]
-	v6Data = Leads1["v6"]["Signal"]
+	v6Data = data[patient]["Leads"]["v6"]["Signal"]
 	while f:
 		RES = []
 		for i in range(batch_size):
@@ -51,6 +54,16 @@ def MyGeteratorData(batch_size):
 			RES.append(res)
 		yield np.array(RES)
 
+
+def get_mask_inverse(mask, SIZE):
+	mask = np.reshape(mask[0], size_of_data)
+	res = []
+	for i in mask:
+		if i ==0:
+			res.append(1.0)
+		else:
+			res.append(0.0)
+	return np.reshape(np.array(res), (1,SIZE,1))
 
 
 #########################################
@@ -91,123 +104,133 @@ ae_filters = {
 }
 
 
+
+latent_pool_size = 7
+def set_architecture(latent_pool_size):
 #############################################
 ##LAYERS
 #############################################
-x_1 = tf.nn.conv1d(x, ae_filters["conv1"], stride=1, padding="SAME",use_cudnn_on_gpu=True) + ae_filters["b_conv1"]
-x_1_mp = tf.layers.max_pooling1d(x_1,pool_size = 2,strides = 2,padding='SAME')
-x_1_relu = tf.nn.leaky_relu(x_1_mp,alpha = 0.2)
-x_2 = tf.nn.conv1d(x_1_relu, ae_filters["conv2"], stride=1, padding="SAME",use_cudnn_on_gpu=True) + ae_filters["b_conv2"]
-x_2_mp = tf.layers.max_pooling1d(x_2,pool_size = 21,strides = 21,padding='SAME')
-x2_relu = tf.nn.leaky_relu(x_2_mp,alpha = 0.2)
-x_3 = tf.nn.conv1d(x2_relu, ae_filters["conv3"], stride=1, padding="SAME",use_cudnn_on_gpu=True) + ae_filters["b_conv3"]
-x_3_mp = tf.layers.max_pooling1d(x_3,pool_size = 2,strides = 2,padding='SAME')
-x3_relu = tf.nn.leaky_relu(x_3_mp,alpha = 0.2)
-# x_4 = tf.nn.conv1d(x3_relu, ae_filters["conv4"], stride=1, padding="SAME",use_cudnn_on_gpu=True) + ae_filters["b_conv4"]
-# x4_relu = tf.nn.leaky_relu(x_4,alpha = 0.2)
-# x_5 = tf.nn.conv1d(x4_relu, ae_filters["conv5"], stride=1, padding="SAME",use_cudnn_on_gpu=True) + ae_filters["b_conv5"]
-# x5_relu = tf.nn.leaky_relu(x_5,alpha = 0.2)
+	x_1 = tf.nn.conv1d(x, ae_filters["conv1"], stride=1, padding="SAME",use_cudnn_on_gpu=True) + ae_filters["b_conv1"]
+	x_1_mp = tf.layers.max_pooling1d(x_1,pool_size = 2,strides = 2,padding='SAME')
+	x1_relu = tf.nn.leaky_relu(x_1_mp,alpha = 0.2)
+	x_2 = tf.nn.conv1d(x1_relu, ae_filters["conv2"], stride=1, padding="SAME",use_cudnn_on_gpu=True) + ae_filters["b_conv2"]
+	x_2_mp = tf.layers.max_pooling1d(x_2,pool_size =latent_pool_size ,strides =latent_pool_size, padding='SAME')
+	x2_relu = tf.nn.leaky_relu(x_2_mp,alpha = 0.2)
+	x_3 = tf.nn.conv1d(x2_relu, ae_filters["conv3"], stride=1, padding="SAME",use_cudnn_on_gpu=True) + ae_filters["b_conv3"]
+	x3_mp = tf.layers.max_pooling1d(x_3,pool_size = 2,strides = 2,padding='SAME')
+	x3_relu = tf.nn.leaky_relu(x3_mp,alpha = 0.2)
+
+
+	x_dec1 = tf.contrib.nn.conv1d_transpose(x3_relu, ae_filters["deconv3"], [batch_size, size_of_data//2//latent_pool_size+1  ,10], stride=2, padding="SAME")
+	x_dec1_relu = tf.nn.leaky_relu(x_dec1,alpha = 0.2)
+	x_dec2 = tf.contrib.nn.conv1d_transpose(x_dec1_relu, ae_filters["deconv4"], [batch_size,2500,10], stride=latent_pool_size, padding="SAME")
+	x_dec2_relu = tf.nn.leaky_relu(x_dec2,alpha = 0.2)
+	x_dec3 = tf.contrib.nn.conv1d_transpose(x_dec2_relu, ae_filters["deconv5"], [batch_size,size_of_data,1], stride=2, padding="SAME")
+	x_dec3_relu = tf.nn.leaky_relu(x_dec3,alpha = 0.2)
+	return x_dec3_relu
 
 
 
-x_dec1 = tf.contrib.nn.conv1d_transpose(x3_relu, ae_filters["deconv3"], [batch_size,120,10], stride=2, padding="SAME")
-x_dec1_relu = tf.nn.leaky_relu(x_dec1,alpha = 0.2)
-x_dec2 = tf.contrib.nn.conv1d_transpose(x2_relu, ae_filters["deconv4"], [batch_size,2500,10], stride=21, padding="SAME")
-x_dec2_relu = tf.nn.leaky_relu(x_dec2,alpha = 0.2)
-x_dec3 = tf.contrib.nn.conv1d_transpose(x_dec2_relu, ae_filters["deconv5"], [batch_size,size_of_data,1], stride=2, padding="SAME")
-x_dec3_relu = tf.nn.leaky_relu(x_dec3,alpha = 0.2)
-# x_dec4 = tf.contrib.nn.conv1d_transpose(x_dec3_relu, ae_filters["deconv5"], [batch_size,size_of_data,1], stride=1, padding="SAME")
-# x_dec4_relu = tf.nn.leaky_relu(x_dec4,alpha = 0.2)
-# x_dec5 = tf.contrib.nn.conv1d_transpose(x_dec4_relu, ae_filters["deconv5"], [batch_size,size_of_data,1], stride=1, padding="SAME")
-# x_dec5_relu = tf.nn.leaky_relu(x_dec5,alpha = 0.2)
-# Q = tf.gradients(x_dec3_relu,x_1)
-Grad1 = tf.gradients(x_dec3_relu,ae_filters["conv1"])
-Grad2 = tf.gradients(x_dec3_relu,ae_filters["conv2"])
-Grad3 = tf.gradients(x_dec3_relu,ae_filters["deconv4"])
-Grad4 = tf.gradients(x_dec3_relu,ae_filters["deconv5"])
+def fit(x_dec3_relu, hm_epochs, patient, count=1):
+	config = tf.ConfigProto(device_count = {'GPU': 1})  # swich between GPU and CPU
+	vars   = tf.trainable_variables() 
+	loss_l2 = tf.add_n([tf.nn.l2_loss(v) for v in vars])*l2_rate
 
-
-
-############################################
-##REGULARIZATION
-############################################
-
-vars   = tf.trainable_variables() 
-loss_l2 = tf.add_n([tf.nn.l2_loss(v) for v in vars])*l2_rate
-
-for i  in range(1):
-#################################################
-##FUNC of ERROR
-##################################################
-	size_of_mask = 801
+	size_of_mask = 900
 	mask,start_mask = Create_mask_static(200,size_of_mask,batch_size)
+	mask_inverse = get_mask_inverse(mask, size_of_data)
 	func_of_error = tf.reduce_mean(tf.square(x_dec3_relu -x)*mask) + loss_l2
+	func_of_error_without_l2 = tf.reduce_mean(tf.square(x_dec3_relu -x)*mask)
+	func_of_error_inside = tf.reduce_mean(tf.square(x_dec3_relu -x)*mask_inverse)
 	optimizer = tf.train.AdagradOptimizer(learning_rate).minimize(func_of_error)
 
-	################################################
-	# LEARNING
-	################################################
 	init = tf.global_variables_initializer()
-	sess = tf.Session()
+	sess = tf.Session(config=config)
 	sess.run(init)
-	ae_filters_copy = sess.run(ae_filters.copy()) # copy for comparing
+	# ae_filters_copy = sess.run(ae_filters.copy()) # copy for comparing
 	# defining batch size, number of epochs and learning rate
-	hm_epochs =4500   # how many times to go through the entire dataset
-	count = 1 # total request in one epoch
 	epox = [-2,-1]
 	error = [100000000000,100000000]
+	error_inside_arr = []
+	error_without_l2_on_epoch_arr = []
 	N=0
+	generator = MyGeteratorData(batch_size, patient)
 	# for epoch in range(hm_epochs):
-	while (abs(error[-1]-error[-2])>0.00001 and N<hm_epochs):
+	while (abs(error[-1]-error[-2])>0.000000001 and N<hm_epochs):
 		epoch_loss_current = 0    # initializing error as 0
+		epoch_loss_current_inside = 0
+		error_without_l2_on_epoch = 0
 		for i in range(count):
-			epoch_x = next(MyGeteratorData(batch_size)) #epoch_x теперь должен содержать картинки
-			_, c = sess.run([optimizer, func_of_error],feed_dict={x_plac: epoch_x})
+			epoch_x = next(generator) #epoch_x теперь должен содержать картинки
+			_, c, error_inside, error_without_l2 = sess.run([optimizer, func_of_error,func_of_error_inside, func_of_error_without_l2],feed_dict={x_plac: epoch_x})
 			epoch_loss_current += c
-		print('Epoch', N, '/', hm_epochs, 'loss:',epoch_loss_current/count)
+			epoch_loss_current_inside+=error_inside/count
+			error_without_l2_on_epoch+=error_without_l2/count
+		#print('Epoch', N, '/', hm_epochs, 'inside_loss:',epoch_loss_current_inside,'\t' ,' outside_loss ',error_without_l2_on_epoch )
+		# print('Epoch', N, '/', hm_epochs, 'loss:',epoch_loss_current/count)
+		error_inside_arr.append(epoch_loss_current_inside)
+		error_without_l2_on_epoch_arr.append(error_without_l2_on_epoch)
 		error.append(epoch_loss_current/count)
 		epox.append(N)
 		N = N + 1
-	#################################################################
-	##SAVER
-	#################################################################
-	# saver = tf.train.Saver()
-	# save_path = saver.save(sess, "AutoincoderSave3/model_autoincoderCNN.ckpt")
-	# print("Model saved in path: %s" % save_path)
+	print(patient+' done')
+	return (epoch_loss_current_inside, error_without_l2_on_epoch)
+
+patients = [rd.choice( list(data.keys())) for i in range(5)] # take a random patients
+for patient in patients:
+	result_table = np.zeros((10,4))
+	for param in [3,7]:
+		for iteration in range(10):
+			x_dec3_relu = set_architecture(param) 
+			epoch_loss_current_inside, error_without_l2_on_epoch = fit(x_dec3_relu, 4000, patient)
+			if(param == 3):
+				result_table[iteration,0] = epoch_loss_current_inside
+				result_table[iteration,1] = error_without_l2_on_epoch
+			else:
+				result_table[iteration,2] = epoch_loss_current_inside
+				result_table[iteration,3] = error_without_l2_on_epoch
+
+	np.savetxt(str(patient)+'.csv',result_table, delimiter = ';')
 
 
-	#####################################
-	##VISUALIZATION
-	#####################################
-	data1 = next(MyGeteratorData(batch_size))
-	plt.subplot(3, 1, 1)
-	plt.plot(range(size_of_data), data1[0])
-	data_output, gradients_output1, gradients_output2,gradients_output3,gradients_output4= sess.run([x_dec3_relu, Grad1,Grad2,Grad3,Grad4], feed_dict={x_plac: data1})
-	plt.subplot(3, 1, 2)
-	# print("data_output", np.shape(data_output))
-	data_output = np.reshape(data_output, [batch_size, size_of_data])
 
-	gradients_output_sorted = np.sort(np.abs(np.reshape(gradients_output1,[1000])))
 
-	gradients_output1 = np.mean(np.abs(np.reshape(gradients_output1,[100,10])))
-	gradients_output2 = np.mean(np.abs(np.reshape(gradients_output2,[80,10,10])))
-	gradients_output3 = np.mean(np.abs(np.reshape(gradients_output3, [80,10,10])))
-	gradients_output4 = np.mean(np.abs(np.reshape(gradients_output4, [100,10])))
-	# gradients_output3 = np.mean(np.abs(np.reshape(gradients_output3,[20,10,5])))
+#####################################
+##VISUALIZATION
+#####################################
+def model_summary():
+	model_vars = tf.trainable_variables()
+	slim.model_analyzer.analyze_vars(model_vars, print_info=True)
 
-	plt.plot(range(size_of_data), data_output[0])
-	plt.plot(range(start_mask,size_of_mask+start_mask), np.zeros(size_of_mask)-300,'ro',markersize = 3)
-	plt.subplot(3,1,3)
-	plt.xlabel(r'$epoch$', fontsize=15, horizontalalignment='right' , x=1)
-	plt.ylabel(r'$Error$', fontsize=15, horizontalalignment='right', y=1)
-	plt.plot(epox[2:-1], error[2:-1],"g", label = "Error")
-	plt.legend()
-	plt.savefig("ecg_photo/vv" +"_"+str(size_of_mask)+ ".png")
-	plt.clf()
-	print("gradients_output1", gradients_output1)
-	print("gradients_output2", gradients_output2)
-	print("gradients_output3", gradients_output3)
-	print("gradients_output4", gradients_output4)
-	plt.figure(2)
-	plt.plot(range(1000),gradients_output_sorted[::-1])
-	plt.show()
+model_summary()
+print("___________________________________________________")
+
+data1 = next(MyGeteratorData(batch_size))
+gridspec.GridSpec(3, 2)
+plt.subplot2grid((3, 2), (0, 0),colspan=2, rowspan=1)
+plt.title("loss out of zone " + str(error_without_l2_on_epoch) +' '+ "loss inside " + str(epoch_loss_current_inside) )
+plt.plot(range(size_of_data), data1[0])
+data_output,Jakob= sess.run([x_dec3_relu, x_1], feed_dict={x_plac: data1})
+print("Jakob_shape",np.shape(Jakob))
+plt.subplot2grid((3, 2), (1, 0),colspan=2, rowspan=1)
+data_output = np.reshape(data_output, [batch_size, size_of_data])
+
+plt.plot(range(size_of_data), data_output[0])
+plt.plot(range(start_mask,size_of_mask+start_mask), np.zeros(size_of_mask)-300,'ro',markersize = 3)
+plt.subplot2grid((3, 2), (2, 0))
+plt.xlabel(r'$epoch$', fontsize=15, horizontalalignment='right' , x=1)
+plt.ylabel(r'$Error$', fontsize=15, horizontalalignment='right', y=1)
+plt.plot(error_inside_arr[50:],"g", label = "Error inside", color = 'g')
+plt.plot(error_without_l2_on_epoch_arr[50:],"g", label = "Error outside ", color = 'b' )
+plt.legend()
+plt.subplot2grid((3, 2), (2, 1))
+plt.xlabel(r'$epoch$', fontsize=15, horizontalalignment='right' , x=1)
+plt.ylabel(r'$Error$', fontsize=15, horizontalalignment='right', y=1)
+plt.plot(error_inside_arr[:50],"g", label = "Error inside", color = 'g')
+plt.plot(error_without_l2_on_epoch_arr[:50],"g", label = "Error outside ", color = 'b' )
+plt.legend()
+plt.show()
+plt.clf()
+
+
+
